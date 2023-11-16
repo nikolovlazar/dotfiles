930133617e87e64a040e2305afa17bdfb45933c8
Lazar Nikolov <hello@nikolovlazar.com>
2023-11-15 19:00:34 -0500

    fixed nvim tmux keybindings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
+vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
+
+-- Moving lines with S-j and S-k when in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Tmux resize panes
+local map = vim.api.nvim_set_keymap
+map("n", "<M-h>", "<cmd>lua require('tmux').resize_left()<cr>", {})
+map("n", "<M-j>", "<cmd>lua require('tmux').resize_down()<cr>", {})
+map("n", "<M-k>", "<cmd>lua require('tmux').resize_up()<cr>", {})
+map("n", "<M-l>", "<cmd>lua require('tmux').resize_right()<cr>", {})
