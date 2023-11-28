4a04f37f5fbca6c8bf8aa5cd207a62d9959a846a
Lazar Nikolov <hello@nikolovlazar.com>
2023-11-28 17:52:20 -0500

    locking vscode-js-debug version

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -115,7 +115,8 @@ return {
       {
         "microsoft/vscode-js-debug",
         -- After install, build it and rename the dist directory to out
-        build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+        build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+        version = "1.*",
       },
       {
         "mxsdev/nvim-dap-vscode-js",
