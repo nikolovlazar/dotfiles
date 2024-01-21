cfafca7c18d9fd6c1b08190a6f74b39ccf2cb689
Lazar Nikolov <hello@nikolovlazar.com>
2024-01-21 10:00:59 -0500

    sync

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -92,15 +92,6 @@ return {
             end,
           },
         },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
       },
       extensions = { "neo-tree", "lazy" },
     }
