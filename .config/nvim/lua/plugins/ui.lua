95ec6aaed433696d279edaacfa167b92ab75ba83
Lazar Nikolov <hello@nikolovlazar.com>
2024-01-23 10:28:10 -0500

    toning down nvim-notify

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -35,6 +35,22 @@ return {
       },
     },
   },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 500,
+      render = "compact",
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.25)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    },
+  },
   -- filename
   {
     "b0o/incline.nvim",
