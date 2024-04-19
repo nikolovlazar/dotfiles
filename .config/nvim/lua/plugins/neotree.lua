0aad4b65d20b610e85e23d102064f31053e59996
Lazar Nikolov <hello@nikolovlazar.com>
2024-04-19 11:43:47 -0400

    update + hide .git and .DS_Store

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -7,5 +7,17 @@ return {
         ["Y"] = "none",
       },
     },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+        },
+        always_show = {
+          ".env",
+        },
+      },
+    },
   },
 }
