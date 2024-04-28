364eb4f5456f54d7df3523886d780557356a5be9
Lazar Nikolov <hello@nikolovlazar.com>
2024-04-28 12:26:45 -0400

    fixing kitty change profile script to work with dotfiles instead

--- .config/kitty/change-profile.sh
+++ .config/kitty/change-profile.sh
@@ -6,12 +6,12 @@ echo "   (2) Screencasting"
 read profile
 
 if [ $profile -eq 1 ]; then
-	rm ~/.config/kitty/kitty.conf
-	cp ~/.config/kitty/kitty-coding.conf ~/.config/kitty/kitty.conf
+	rm ~/dotfiles/.config/kitty/kitty.conf
+	cp ~/dotfiles/.config/kitty/kitty-coding.conf ~/dotfiles/.config/kitty/kitty.conf
 	echo "Get in the flow!"
 elif [ $profile -eq 2 ]; then
-	rm ~/.config/kitty/kitty.conf
-	cp ~/.config/kitty/kitty-screencasting.conf ~/.config/kitty/kitty.conf
+	rm ~/dotfiles/.config/kitty/kitty.conf
+	cp ~/dotfiles/.config/kitty/kitty-screencasting.conf ~/dotfiles/.config/kitty/kitty.conf
 	echo "Have fun recording!"
 else
 	echo "What the hell?"
