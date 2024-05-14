bc6f476cca3b10af346030e92fe9938092b287db
Lazar Nikolov <hello@nikolovlazar.com>
2024-05-14 12:21:50 -0400

    enabling transparency

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,17 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "moon" },
+    opts = {
+      style = "moon",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_colors = function(colors)
+        colors.bg_statusline = colors.none
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -23,7 +23,7 @@ return {
 
     return {
       options = {
-        theme = "auto",
+        theme = "tokyonight",
         globalstatus = true,
         disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
       },
