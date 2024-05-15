5f2d15b744de91249a567bd89d787b2e27e48ed7
Lazar Nikolov <hello@nikolovlazar.com>
2024-05-15 14:08:48 -0400

    kitty scripts fix

--- .config/kitty/change-profile.sh
+++ .config/kitty/change-profile.sh
@@ -3,13 +3,13 @@
 echo "Select a profile:"
 echo "   (1) Coding"
 echo "   (2) Screencasting"
-read profile
+read -r profile
 
-if [ $profile -eq 1 ]; then
+if [ "$profile" -eq 1 ]; then
 	rm ~/dotfiles/.config/kitty/kitty.conf
 	cp ~/dotfiles/.config/kitty/kitty-coding.conf ~/dotfiles/.config/kitty/kitty.conf
 	echo "Get in the flow!"
-elif [ $profile -eq 2 ]; then
+elif [ "$profile" -eq 2 ]; then
 	rm ~/dotfiles/.config/kitty/kitty.conf
 	cp ~/dotfiles/.config/kitty/kitty-screencasting.conf ~/dotfiles/.config/kitty/kitty.conf
 	echo "Have fun recording!"
