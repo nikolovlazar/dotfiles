0832a0fdfc34bbabdcec35a282e74fd3200d7a24
Lazar Nikolov <hello@nikolovlazar.com>
2024-06-20 17:51:37 -0400

    adding vscode settings + modify nvim-cmp

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
   },
