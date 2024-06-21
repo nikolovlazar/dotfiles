c80457db5c13b112da2d35bb5e8cbd2d4e106fa5
Lazar Nikolov <hello@nikolovlazar.com>
2024-06-21 08:35:59 -0400

    disable "no information available" notification on hover

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,3 +11,18 @@ opt.cmdheight = 0
 vim.g.root_spec = { "cwd" }
 vim.g.omni_sql_no_default_maps = 1
 vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
+
+-- Disable "No information available" notification on hover
+vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
+  config = config or {}
+  config.focus_id = ctx.method
+  if not (result and result.contents) then
+    return
+  end
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+  return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
+end
