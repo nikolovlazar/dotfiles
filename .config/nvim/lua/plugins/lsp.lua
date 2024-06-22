867c24203c4cee689f50731a26c894b3c1b9e5b3
Lazar Nikolov <hello@nikolovlazar.com>
2024-06-21 21:13:31 -0400

    added border on hover window

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,30 @@
+-- Disable "No information available" notification on hover
+-- plus define border for hover window
+vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
+  config = config or {
+    border = {
+      { "╭", "Comment" },
+      { "─", "Comment" },
+      { "╮", "Comment" },
+      { "│", "Comment" },
+      { "╯", "Comment" },
+      { "─", "Comment" },
+      { "╰", "Comment" },
+      { "│", "Comment" },
+    },
+  }
+  config.focus_id = ctx.method
+  if not (result and result.contents) then
+    return
+  end
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+  return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
