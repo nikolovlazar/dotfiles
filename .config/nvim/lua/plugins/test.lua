36b28cd5939f931dea78d972fda5a21b2552e2c0
Lazar Nikolov <hello@nikolovlazar.com>
2024-07-15 09:52:11 -0400

    adding testing keymaps

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -2,7 +2,20 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = { "nvim-neotest/neotest-jest", "marilari88/neotest-vitest" },
-    keys = {},
+    keys = {
+      {
+        "<leader>tL",
+        function()
+          require("neotest").run.run_last({ strategy = "dap" })
+        end,
+        desc = "Debug Last Test",
+      },
+      {
+        "<leader>tw",
+        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+        desc = "Run Watch",
+      },
+    },
     opts = function(_, opts)
       table.insert(
         opts.adapters,
