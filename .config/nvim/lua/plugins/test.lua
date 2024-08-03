3ffa32fd7f81362aa29e97ef9159e9e1f42e6915
Lazar Nikolov <hello@nikolovlazar.com>
2024-08-03 19:28:27 -0400

    removed extra neotest-jest config

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -3,17 +3,7 @@ return {
     "nvim-neotest/neotest",
     dependencies = { "nvim-neotest/neotest-jest", "marilari88/neotest-vitest" },
     opts = function(_, opts)
-      table.insert(
-        opts.adapters,
-        require("neotest-jest")({
-          jestCommand = "npm test --",
-          jestConfigFile = "custom.jest.config.ts",
-          env = { CI = true },
-          cwd = function()
-            return vim.fn.getcwd()
-          end,
-        })
-      )
+      table.insert(opts.adapters, require("neotest-jest"))
       table.insert(opts.adapters, require("neotest-vitest"))
     end,
   },
