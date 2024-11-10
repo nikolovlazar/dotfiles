4d2e5525cc9dd3e92479b6e0feb06cbfeae1b574
Lazar Nikolov <hello@nikolovlazar.com>
2024-11-10 06:33:54 -0500

    bringing back neotree position tor ight

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
-      position = "left",
+      position = "right",
       width = 30,
       mappings = {
         ["Y"] = "none",
