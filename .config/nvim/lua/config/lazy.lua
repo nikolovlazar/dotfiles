433b20ba042eb15c81cccaeac28ba2315ebd149c
Lazar Nikolov <hello@nikolovlazar.com>
2025-01-03 14:41:11 -0500

    disable lazyvim update notification

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -49,7 +49,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = {} },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, notify = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
