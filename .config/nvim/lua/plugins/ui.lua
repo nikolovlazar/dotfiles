e0c6bddc7c7e5a3ae12da0a0a01c67c234b2e671
Lazar Nikolov <hello@nikolovlazar.com>
2025-01-04 10:34:42 -0500

    changing notifier timeout to 500ms

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -23,19 +23,12 @@ return {
     },
   },
   {
-    "rcarriga/nvim-notify",
+    "folke/snacks.nvim",
     opts = {
-      timeout = 500,
-      render = "compact",
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.25)
-      end,
-      on_open = function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end,
+      notifier = {
+        timeout = 500,
+        style = "compact",
+      },
     },
   },
   -- filename
