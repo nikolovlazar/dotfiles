d95fc5f896aac872da67718fbb9b0df04defe533
Lazar Nikolov <hello@nikolovlazar.com>
2025-01-18 13:42:57 -0500

    cursorline false

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -54,7 +54,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
