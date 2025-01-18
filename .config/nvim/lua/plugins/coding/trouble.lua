43940ed0b57fff3c8435d03b0f4b97fb4fbc2dde
Lazar Nikolov <hello@nikolovlazar.com>
2025-01-18 12:46:36 -0500

    trouble to replace aerial + more

---
+++ .config/nvim/lua/plugins/coding/trouble.lua
@@ -0,0 +1,39 @@
+return {
+  {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>cD",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cd",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Document Symbols",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+     desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>cL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>cQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+}
+}
