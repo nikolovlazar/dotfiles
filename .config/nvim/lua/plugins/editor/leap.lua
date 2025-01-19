03cd84d24cdf7efc698af998ab02e5294e44e997
Lazar Nikolov <hello@nikolovlazar.com>
2025-01-18 21:29:42 -0500

    removed redundant type

--- .config/nvim/lua/plugins/editor/leap.lua
+++ .config/nvim/lua/plugins/editor/leap.lua
@@ -3,7 +3,6 @@ return {
     'ggandor/flit.nvim',
     enabled = true,
     keys = function()
-      ---@type LazyKeysSpec[]
       local ret = {}
       for _, key in ipairs { 'f', 'F', 't', 'T' } do
         ret[#ret + 1] = { key, mode = { 'n', 'x', 'o' } }
