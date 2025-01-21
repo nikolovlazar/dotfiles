bf3c9875dc1fca7fdc3e32b25f2042b901324cd4
Lazar Nikolov <hello@nikolovlazar.com>
2025-01-21 07:47:49 -0500

    next and prev references keymap

--- .config/nvim/lua/plugins/coding/lspconfig.lua
+++ .config/nvim/lua/plugins/coding/lspconfig.lua
@@ -91,7 +91,14 @@ return {
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
+
+          map(']]', function()
+            Snacks.words.jump(vim.v.count1)
+          end, 'Next Reference')
+          map('[[', function()
+            Snacks.words.jump(-vim.v.count1)
+          end, 'Prev Reference')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
