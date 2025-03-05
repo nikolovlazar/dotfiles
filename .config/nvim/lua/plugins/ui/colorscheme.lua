96e176b3ebe2be281a8c4c2c002cc069d63bca8e
Lazar Nikolov <hello@nikolovlazar.com>
2025-03-05 07:58:06 -0500

    bringing back cyberdream

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,24 +1,15 @@
 return {
   {
-    'ricardoraposo/nightwolf.nvim',
+    'scottmckendry/cyberdream.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd [[colorscheme nightwolf]]
-      vim.api.nvim_set_hl(0, 'FoldColumn', { fg = '#9696ff' })
+      require('cyberdream').setup {
+        transparent = true,
+        italic_comments = true,
+        borderless_telescope = false,
+      }
+      vim.cmd [[colorscheme cyberdream]]
     end,
   },
-  -- {
-  --   'scottmckendry/cyberdream.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('cyberdream').setup {
-  --       transparent = true,
-  --       italic_comments = true,
-  --       borderless_telescope = false,
-  --     }
-  --     vim.cmd [[colorscheme cyberdream]]
-  --   end,
-  -- },
 }
