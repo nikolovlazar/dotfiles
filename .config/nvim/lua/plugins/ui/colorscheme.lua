d6208b28c3e936ce6024b2f64cf9bb8bbaccab44
Lazar Nikolov <lazar.nikolov@sentry.io>
2025-03-06 16:05:51 -0500

    switching cyberdream for transparent catppuccin

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,15 +1,64 @@
 return {
   {
-    'scottmckendry/cyberdream.nvim',
-    lazy = false,
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      require('cyberdream').setup {
-        transparent = true,
-        italic_comments = true,
-        borderless_telescope = false,
+      require('catppuccin').setup {
+        transparent_background = true,
+        integrations = {
+          aerial = true,
+          alpha = true,
+          cmp = true,
+          dashboard = true,
+          flash = true,
+          fzf = true,
+          grug_far = true,
+          gitsigns = true,
+          headlines = true,
+          illuminate = true,
+          indent_blankline = { enabled = true },
+          leap = true,
+          lsp_trouble = true,
+          mason = true,
+          markdown = true,
+          mini = true,
+          native_lsp = {
+            enabled = true,
+            underlines = {
+              errors = { 'undercurl' },
+              hints = { 'undercurl' },
+              warnings = { 'undercurl' },
+              information = { 'undercurl' },
+            },
+          },
+          navic = { enabled = true, custom_bg = 'lualine' },
+          neotest = true,
+          neotree = true,
+          noice = true,
+          notify = true,
+          semantic_tokens = true,
+          snacks = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+          which_key = true,
+        },
       }
-      vim.cmd [[colorscheme cyberdream]]
+      vim.cmd [[colorscheme catppuccin]]
     end,
   },
+  -- {
+  --   'scottmckendry/cyberdream.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('cyberdream').setup {
+  --       transparent = true,
+  --       italic_comments = true,
+  --       borderless_telescope = false,
+  --     }
+  --     vim.cmd [[colorscheme cyberdream]]
+  --   end,
+  -- },
 }
