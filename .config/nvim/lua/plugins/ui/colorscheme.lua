3da056cbcf3a8ab2ce94bde711c52346d605491d
Lazar Nikolov <lazar.nikolov@sentry.io>
2025-06-20 14:00:32 -0400

    light/dark mode support

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,59 +1,25 @@
 return {
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   priority = 1000,
-  --   config = function()
-  --     require('catppuccin').setup {
-  --       transparent_background = true,
-  --       integrations = {
-  --         aerial = true,
-  --         alpha = true,
-  --         cmp = true,
-  --         dashboard = true,
-  --         flash = true,
-  --         fzf = true,
-  --         grug_far = true,
-  --         gitsigns = true,
-  --         headlines = true,
-  --         illuminate = true,
-  --         indent_blankline = { enabled = true },
-  --         leap = true,
-  --         lsp_trouble = true,
-  --         mason = true,
-  --         markdown = true,
-  --         mini = true,
-  --         native_lsp = {
-  --           enabled = true,
-  --           underlines = {
-  --             errors = { 'undercurl' },
-  --             hints = { 'undercurl' },
-  --             warnings = { 'undercurl' },
-  --             information = { 'undercurl' },
-  --           },
-  --         },
-  --         navic = { enabled = true, custom_bg = 'lualine' },
-  --         neotest = true,
-  --         neotree = true,
-  --         noice = true,
-  --         notify = true,
-  --         semantic_tokens = true,
-  --         snacks = true,
-  --         telescope = true,
-  --         treesitter = true,
-  --         treesitter_context = true,
-  --         which_key = true,
-  --       },
-  --     }
-  --     vim.cmd [[colorscheme catppuccin]]
-  --   end,
-  -- },
+  {
+    'eliseshaffer/darklight.nvim',
+    config = function()
+      require('darklight').setup {
+        mode = 'custom',
+        light_mode_callback = function()
+          vim.o.background = 'light'
+        end,
+        dark_mode_callback = function()
+          vim.o.background = 'dark'
+        end,
+      }
+    end,
+  },
   {
     'scottmckendry/cyberdream.nvim',
     lazy = false,
     priority = 1000,
     config = function()
       require('cyberdream').setup {
+        variant = 'auto',
         transparent = true,
         italic_comments = true,
         borderless_telescope = false,
