0eca3dc5570491b6ee01466b422b70f44819aa37
Lazar Nikolov <hello@nikolovlazar.com>
2025-07-17 08:54:27 -0400

    treesitter set config files to conf filetype

--- .config/nvim/lua/plugins/coding/treesitter.lua
+++ .config/nvim/lua/plugins/coding/treesitter.lua
@@ -52,6 +52,13 @@ return {
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    config = function()
+      vim.filetype.add {
+        pattern = {
+          ['config'] = 'conf',
+        },
+      }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
