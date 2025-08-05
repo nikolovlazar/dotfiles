85aa1324ee78adb5651da012cb3fb0fba74ba44e
Lazar Nikolov <hello@nikolovlazar.com>
2025-08-05 07:15:44 -0400

    raycast: new video project structure script

---
+++ .config/raycast/scripts/new-video-project.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New Video Project Structure
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon 📂
+# @raycast.author nikolovlazar
+# @raycast.authorURL https://raycast.com/nikolovlazar
+# @raycast.description Creates a folder structure for a new video project
+# @raycast.argument1 { "type": "text", "placeholder": "slug" }
+
+date=$(date "+%Y%m%d")
+mkdir ~/Movies/${date}-${1//}
+cd ~/Movies/${date}-${1//}
+
+folders=("FOOTAGE" "AUDIO" "MUSIC" "GRAPHICS" "AE" "SEQUENCES" "EXPORTS")
+
+for ((i = 0; i < ${#folders[@]}; i++)); do
+	index=$((i+1))
+	dirname=$(printf "%02d %s" "$index" "${folders[$i]}")
+	mkdir "$dirname"
+done
