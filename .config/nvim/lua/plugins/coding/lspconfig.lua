d357a4f36c1cc9b45bd22afd827bf42135f4c9d0
Lazar Nikolov <hello@nikolovlazar.com>
2025-12-14 16:28:08 -0500

    adding biome to mason

--- .config/nvim/lua/plugins/coding/lspconfig.lua
+++ .config/nvim/lua/plugins/coding/lspconfig.lua
@@ -257,6 +257,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'astro-language-server',
+        'biome',
         'css-lsp',
         'debugpy',
         'delve',
