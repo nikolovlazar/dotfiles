c71ad7dc09ecb4b63b7e4ccf7afe21d50172386b
Lazar Nikolov <hello@nikolovlazar.com>
2025-12-14 16:23:50 -0500

    adding avante.nvim

--- .config/nvim/lua/lazy-init.lua
+++ .config/nvim/lua/lazy-init.lua
@@ -10,6 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require('lazy').setup({
   spec = {
     'tpope/vim-sleuth',
+    { import = 'plugins.coding.ai' },
     { import = 'plugins.coding.autopairs' },
     { import = 'plugins.coding.cmp' },
     { import = 'plugins.coding.lspconfig' },
