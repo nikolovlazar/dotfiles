83946694f31e80059e1f68dd01b0d5e977d25543
Lazar Nikolov <hello@nikolovlazar.com>
2025-12-14 15:58:03 -0500

    nvim: relaxing hardtime by allowing hjkl

--- .config/nvim/lua/plugins/editor/hardtime.lua
+++ .config/nvim/lua/plugins/editor/hardtime.lua
@@ -3,6 +3,13 @@ return {
     'm4xshen/hardtime.nvim',
     lazy = false,
     dependencies = { 'MunifTanjim/nui.nvim' },
-    opts = {},
+    opts = {
+      restricted_keys = {
+        ['h'] = false,
+        ['j'] = false,
+        ['k'] = false,
+        ['l'] = false,
+      },
+    },
   },
 }
