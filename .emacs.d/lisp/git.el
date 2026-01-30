60c90809743ebe10f11e533a8e6e332dd4503992
Lazar Nikolov <hello@nikolovlazar.com>
2026-01-30 12:53:53 -0500

    emacs: show remote in magit status

--- .emacs.d/lisp/git.el
+++ .emacs.d/lisp/git.el
@@ -4,4 +4,13 @@
   :ensure t
   :bind ("C-c g" . magit-status))
 
+(setq magit-status-headers-hook
+      '(magit-insert-error-header
+        magit-insert-diff-filter-header
+        magit-insert-head-branch-header
+        magit-insert-upstream-branch-header
+        magit-insert-push-branch-header
+        magit-insert-tags-header
+        magit-insert-remote-header))
+
 (provide 'git)
